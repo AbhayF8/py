1442eaf2c81cdd810b413bb4a6daba796a1b343e
Abhay Patel <patelabhayindia@gmail.com>
2022-04-14 13:56:23 +0530

    Day 2

---
+++ 001.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!");
+
+
+    int a,b;
+    {
+        printf("Enter no. a\n> ");
+        scanf("%d",&a);
+
+        printf("Enter no. b\n> ");
+        scanf("%d",&b);
+
+        printf("Your sum is %d",a+b);
+
+    }}
