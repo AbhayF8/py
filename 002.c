e56f7641f9cf370e625917db6d473fc38a4eef76
Abhay Patel <patelabhayindia@gmail.com>
2022-04-15 17:56:33 +0530

    Day 3

---
+++ 002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    printf("The size of double is ");
+    printf("%lu",sizeof(double));
+    printf("\nThe size of float is ");
+    printf("%lu",sizeof(float));
+    printf("\nThe size of char is ");
+    printf("%lu",sizeof(char));
+    printf("\nThe size of int is ");
+    printf("%lu",sizeof(int));
+    printf("\nThe size of long int is ");
+    printf("%lu",sizeof(long int));
+    printf("\nThe size of long double is ");
+    printf("%lu",sizeof(long double));
+    
+}
