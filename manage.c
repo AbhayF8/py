71e8edab27a5894bc9f276a85830501015d0c05a
Abhay Patel <patelabhayindia@gmail.com>
2022-05-27 11:13:41 +0530

    Day 39

---
+++ manage.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Problem Statement:-
+
+Suppose ABC is a private limited company which manages the employee records of other companies. Employee id can be of any length, and it can contain any character. The following are the task you have to perform for three employees.
+
+    Take the length of an employee id as an input and store it in an integer length variable
+    Take an employee id as an input and display it on the screen.
+    Save the employee id in a character array, which is allocated dynamically.
+    Create only one character array dynamically.
+
+Program Example:-
+
+Employee1;
+
+Enter the no. of characters in your employee id: 45
+
+//dynamically allocate the character array
+
+Take input from the user: //employee_id
+
+Employee2;
+
+Enter the no. of characters in your employee id: 5
+
+//dynamically allocate the character array
+
+Take input from the user: //employee_id
+
+Employee3;
+
+Enter the no. of characters in your employee id: 9
+
+//dynamically allocate the character array
+
+Take input from the user: //employee_id
+
+You have to reallocate the memory in such a way so that your program is robust and uses less memory.
+*/
+
+void manage() {
+	int len;
+	char* ptr;
+	printf("Employee 1:\nWhat is the length of your eID?\n");
+	scanf("%d",&len);
+	ptr=(char*)malloc(len*sizeof(char));
+	printf("Enter your eID\n> ");
+	for (int i=0;i<len;i++) {
+		printf("Enter your %d character\n",i);
+		scanf("%s",&ptr[i]);
+	}
+	printf("Your eID is: \n");
+	for (int i=0;i<len;i++) {
+		printf("%c",ptr[i]);
+	}
+
+	printf("\n\nEmployee 2:\nWhat is the length of your eID?\n");
+	scanf("%d",&len);
+	ptr=(char*)realloc(ptr,len*sizeof(char));
+	printf("Enter your eID\n> ");
+	for (int i=0;i<len;i++) {
+		printf("Enter your %d character\n",i);
+		scanf("%s",&ptr[i]);
+	}
+	printf("Your eID is: \n");
+	for (int i=0;i<len;i++) {
+		printf("%c",ptr[i]);
+	}
+	
+	printf("\n\nEmployee 3:\nWhat is the length of your eID?\n");
+	scanf("%d",&len);
+	ptr=(char*)realloc(ptr,len*sizeof(char));
+	printf("Enter your eID\n> ");
+	for (int i=0;i<len;i++) {
+		printf("Enter your %d character\n",i);
+		scanf("%s",&ptr[i]);
+	}
+	printf("Your eID is: \n");
+	for (int i=0;i<len;i++) {
+		printf("%c",ptr[i]);
+	}
+}
+
+int main() {
+	manage();
+}
