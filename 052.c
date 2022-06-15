ecb6992cec88135624f0d624d4a9e5dd582fec28
Abhay Patel <patelabhayindia@gmail.com>
2022-06-16 00:15:51 +0530

    Day 58

--- 052.c
+++ 052.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /* Suppose you have a template letter.txt. You have to fill in values to a template. Letter.txt looks something like this:
 
@@ -20,22 +21,35 @@ int main() {
 	ptr=fopen("/tmp/template.txt","w");
 	fprintf(ptr,"%s",template);
 	fclose(ptr);
-	return 0;
 	char name[20],item[10],outlet[25];
 	printf("Enter your name\n");
-	scanf("%s",&name);
+	fgets(name,20,stdin);
 	printf("What did u bought\n");
-	scanf("%s",&item);
+	fgets(item,10,stdin);
 	printf("Enter your outlet\n");
-	scanf("%s",&outlet);
-	ptr=fopen("/tmp/template.txt","rw");
-	while (fscanf(ptr,"%s",string)!="\n") {
-		if (string=="{{name}}")
-			fprintf(ptr,"%s",name);
-		else if(string=="{{item}}")
-			fprintf(ptr,"%s",item);
-		else if(string=="{{outlet}}")
-			fprintf(ptr,"%s",outlet);
+	fgets(outlet,20,stdin);
+	FILE *ptr1=NULL;
+	ptr1=fopen("/tmp/out.txt","a");
+	ptr=fopen("/tmp/template.txt","r");
+	fscanf(ptr,"%s",string);
+	while (strcmp(string,"\n")!=0) {
+		if (strcmp(string,"{{name}}")==0) {
+			fprintf(ptr1,"%s ",name);
+			printf("Case 1 is running\n");
+		}
+		else if(strcpy(string,"{{item}}")==0) {
+			fprintf(ptr1,"%s ",item);
+			printf("Case 2 is running\n");
+		}
+		else if(strcmp(string,"{{outlet}}")==0) {
+			fprintf(ptr1,"%s ",outlet);
+			printf("Case 3 is running\n");
+		}
+		else {
+			fprintf(ptr1,"%s ",string);
+		}
+		fscanf(ptr,"%s",string);
 		}
 	fclose(ptr);
+	fclose(ptr1);
 }
